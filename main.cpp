73c8a04e7fe871a19ed2fb4bccdf514b4bf18141
FloraChang <hiyaflora@gmail.com>
2016-07-15 11:21:48 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void reverse( char* cstring );
+
+int main(int argc, char *argv[])
+{
+    char cstring[256];
+    cout<<"enter a string to reverse: "<<endl;
+    cin>>cstring;
+    cout<<cstring<<endl;
+    reverse( cstring );
+    cout<< cstring<<endl;
+}
+
+void reverse( char* cstring )
+{
+    int len = (int)strlen(cstring) + 1; // get the length of the c-type string
+    char *temp = new char[len];
+    int i, j;
+    for( i = len-2, j = 0; i >= 0 && j < len ; i--, j++ )
+    {
+        temp[j] = cstring[i];
+    }
+    
+    temp[len-1]='\0';
+    
+    for(int i= 0; i<len;i++){
+        cstring[i] = temp[i];
+    }
+    delete []temp;
+}
